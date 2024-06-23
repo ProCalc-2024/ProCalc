d76c5e39997ead0d21bbe2f8092986e1e58dd8a7
ProCalc <167237786+ProCalc-2024@users.noreply.github.com>
2024-06-23 17:47:07 -0300

    main.py

--- main.py
+++ main.py
@@ -1,32 +1,42 @@
 import streamlit as st 
-import login
-from streamlit_option_menu import option_menu
-import streamlit_authenticator as stauth
-import time
 
-# Funções para cada página
-def pagina_inicial():
-    st.title("Página Inicial")
-    st.write("Bem-vindo à Página Inicial!")
+def Tela_login():
 
-def pagina_sobre():
-    st.title("Sobre")
-    st.write("Esta é a página Sobre.")
+    col1, col2, col3 = st.columns([1, 4, 1])
 
-def pagina_contato():
-    st.title("Contato")
-    st.write("Esta é a página de Contato.")
+    with col2:
+        with st.form("lo1"):
 
-# Criar menu de navegação
-paginas = {
-    "Página Inicial": pagina_inicial,
-    "Sobre": pagina_sobre,
-    "Contato": pagina_contato
-}
+            st.title("ProCalc")
 
-# Adicionar um seletor para o menu de navegação
-escolha = st.sidebar.selectbox("Navegação", list(paginas.keys()))
+            email = st.text_input("Email", placeholder= "digite aqui seu Email")
+            senha = st.text_input("Senha", placeholder= "digite aqui sua Senha", type="password")
 
-# Executar a função da página selecionada
-paginas[escolha]()
+            st.write("")
+
+            buton_login = st.form_submit_button("Logar", type="primary")
+
+            buton_cadastrar = st.form_submit_button("Cadastrar", type="primary")
+
+            st.write(buton_cadastrar)
+
+            return buton_cadastrar
+
+
+
+
+def Tela_cadastro():
+
+    col1, col2, col3 = st.columns([1, 4, 1])
+
+    with col2:
+        with st.form("lo2"):
+
+            st.title("Registro")
+
+            usuario = st.text_input("Nome", placeholder= "digite aqui seu Nome")
+            email = st.text_input("Email", placeholder= "digite aqui seu Email")
+            senha = st.text_input("Senha", placeholder= "digite aqui sua Senha", type="password")
+
+            buton_cadastrar = st.form_submit_button("Cadastrar")
 
