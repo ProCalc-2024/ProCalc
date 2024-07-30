1f851949e89ccd8dab4ec65956444dae08dc5e14
ProCalc <167237786+ProCalc-2024@users.noreply.github.com>
2024-07-30 12:41:03 -0300

    Update save_questoes.py

--- save_questoes.py
+++ save_questoes.py
@@ -4,7 +4,7 @@ from yaml.loader import SafeLoader
 import numpy as np
 from github import Github
 
-GITHUB_TOKEN = "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"
+GITHUB_TOKEN = "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"
 REPO_NAME = "ProCalc-2024/ProCalc"
 ARQUIVO_PATH = "questoes.yaml"
 COMMIT_MESSAGE = "Atualização via Streamlit"
