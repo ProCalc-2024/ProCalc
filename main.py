135e81f2770329daa33ce9d6325a85ed738aa441
ProCalc <167237786+ProCalc-2024@users.noreply.github.com>
2024-08-12 10:59:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -15,11 +15,6 @@ local_css(r"styles.css")
 with open('config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
-with open('.github/workflows/senha.yml') as fil:
-    con = yaml.load(fil, Loader=SafeLoader)
-
-st.write(con)
-
 col1, col2, col3, col4 = st.columns([1, 1, 1, 1])
 
 authenticator = stauth.Authenticate(
