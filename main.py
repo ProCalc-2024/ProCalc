afc37dc7d058371dd6c7875c0d2370917eae9751
ProCalc <167237786+ProCalc-2024@users.noreply.github.com>
2024-08-13 15:04:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import save_questoes
 import questoes
 import os
 
-os.environ['NOME_DA_VARIAVEL'] = ${{ secrets.MY_SECRET_PASSWORD }}
+os.environ['MY_SECRET_PASSWORD'] 
 
 def local_css(file_name):
         with open(file_name) as f:
