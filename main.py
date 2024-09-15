407f80d7dc698bffdae0cc7a8eb73ec6489ab83d
ProCalc <167237786+ProCalc-2024@users.noreply.github.com>
2024-09-15 20:50:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from google.oauth2.service_account import Credentials
 
 # Carregue as credenciais diretamente do secrets do Streamlit
 credentials = Credentials.from_service_account_info(
-    st.secrets['gsheets']
+    st.secrets["gsheets"]
 )
 
 # Autenticação e conexão com a planilha
 client = gspread.authorize(credentials)
 
 # Acesse a planilha
-sheet = client.open("Bd_ProCalc").sheet1
+sheet = client.open('Bd_ProCalc').sheet1
 
 # Leia dados da planilha
 df = sheet.get_all_records()
@@ -22,6 +22,12 @@ st.write(df)
 # Interface para entrada de novos dados
 enunciado = st.text_input('Enunciado')
 assunto = st.text_input('Assunto')
+
+# Adicione nova linha na planilha quando clicar no botão
+if st.button('Adicionar na planilha'):
+    sheet.append_row([enunciado, assunto])
+    st.success('Linha adicionada com sucesso!')
+
 
 # Adicione nova linha na planilha quando clicar no botão
 if st.button('Adicionar na planilha'):
