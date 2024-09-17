3c2d55e008e877f1a6e2d2c63b8a6967805e9c43
ProCalc <167237786+ProCalc-2024@users.noreply.github.com>
2024-09-17 12:02:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,19 @@ from streamlit_gsheets import GSheetsConnection
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 # Demo Births DataFrame
-df = psql.load_births()
+df = pd.DataFrame({
+        'OrderID': [101, 102, 103, 104, 105],
+        'CustomerName': ['Alice', 'Bob', 'Charlie', 'David', 'Eve'],
+        'ProductList': ['ProductA, ProductB', 'ProductC', 'ProductA, ProductC', 'ProductB, ProductD', 'ProductD'],
+        'TotalPrice': [200, 150, 250, 300, 100],
+        'OrderDate': ['2023-08-18', '2023-08-19', '2023-08-19', '2023-08-20', '2023-08-20']
+    })
 
 # click button to update worksheet
 # This is behind a button to avoid exceeding Google API Quota
 if st.button("Create new worksheet"):
     df = conn.create(
-        worksheet="Example 1",
+        worksheet="Questões",
         data=df,
     )
     st.cache_data.clear()
