7ed2fa1eff64598536fa8f11576554b09b141fbd
ProCalc <167237786+ProCalc-2024@users.noreply.github.com>
2024-09-20 16:57:10 -0300

    Update save_questoes.py

--- save_questoes.py
+++ save_questoes.py
@@ -29,17 +29,17 @@ def inserir_ques():
     letra_e = st.text_input("Resposta5", placeholder= "digite aqui a resposta5")
 
     conn = st.connection("gsheets", type=GSheetsConnection)
-    updated_orders = conn.copy()
+    
     
     novo = ({
-        'Materia': [materia] + updated_orders['Materia'],
-        'Descrição': [descricao] + updated_orders['Descrição'],
-        'Enunciado': [enunciado] + updated_orders['Enunciado'],
-        'Alternativa_A': [letra_a] + updated_orders['Alternativa_A'],
-        'Alternativa_B': [letra_b] + updated_orders['Alternativa_B'],
-        'Alternativa_C': [letra_c] + updated_orders['Alternativa_C'],
-        'Alternativa_D': [letra_d] + updated_orders['Alternativa_D'],
-        'Alternativa_E': [letra_e] + updated_orders['Alternativa_E']
+        'Materia': [materia] + conn['Materia'],
+        'Descrição': [descricao] + conn['Descrição'],
+        'Enunciado': [enunciado] + conn['Enunciado'],
+        'Alternativa_A': [letra_a] + conn['Alternativa_A'],
+        'Alternativa_B': [letra_b] + conn['Alternativa_B'],
+        'Alternativa_C': [letra_c] + conn['Alternativa_C'],
+        'Alternativa_D': [letra_d] + conn['Alternativa_D'],
+        'Alternativa_E': [letra_e] + conn['Alternativa_E']
     })
     
     novo = pd.DataFrame(novo)
