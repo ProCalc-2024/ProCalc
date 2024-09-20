d1b2c6544fb4dbe6665186fbbdbd57a7fb3e73cc
ProCalc <167237786+ProCalc-2024@users.noreply.github.com>
2024-09-20 14:53:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,32 @@ from streamlit_gsheets import GSheetsConnection
 # Create GSheets connection
 st.title("Google Sheets as a DataBase")
 
+def inserir_ques():    
+
+    # adicionar uma nova pergunta
+    result = {}
+
+    col1, col2 = st.columns([1, 1])
+
+    lista = [1,2,3]
+
+    with col2:
+        assunto = st.selectbox("selecione uma materia", lista)
+
+    with col1:
+        descricao = st.text_input("descrição")
+
+    enunciado = st.text_area("Enunciado", placeholder= "digite aqui o enunciado da questão")
+    letra_a = st.text_input("Resposta1", placeholder= "digite aqui a resposta correta") 
+    letra_b = st.text_input("Resposta2", placeholder= "digite aqui a resposta2")
+    letra_c = st.text_input("Resposta3", placeholder= "digite aqui a resposta3") 
+    letra_d = st.text_input("Resposta4", placeholder= "digite aqui a resposta4") 
+    letra_e = st.text_input("Resposta5", placeholder= "digite aqui a resposta5")
+
+    if st.button("Salvar"):   
+        
+        st.success("Questão salva")
+
 # Function to create a sample Orders dataframe
 def create_orders_dataframe():
     return pd.DataFrame({
