c11c11090319898fd4c7251e8eb7515e97bc6afd
ProCalc <167237786+ProCalc-2024@users.noreply.github.com>
2024-09-20 17:28:09 -0300

    Update save_questoes.py

--- save_questoes.py
+++ save_questoes.py
@@ -29,7 +29,7 @@ def inserir_ques():
     letra_e = st.text_input("Resposta5", placeholder= "digite aqui a resposta5")
 
     conn = st.connection("gsheets", type=GSheetsConnection)
-    sheet = conn.write(worksheet="Questões")
+    sheet = conn.read(worksheet="Questões")
     
     novo = ({
         'Materia': [materia] + sheet['Materia'],
