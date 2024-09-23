0eeee0c779274f52cf3e9c8db0878fb943220ec2
ProCalc <167237786+ProCalc-2024@users.noreply.github.com>
2024-09-23 12:02:32 -0300

    Update questoes.py

--- questoes.py
+++ questoes.py
@@ -23,7 +23,7 @@ def read_questao():
     resul = {}
 
     # lista de materias
-    lista = [linha["Materia"] for linha in dict if linha["Materia"] == "rdsf"]
+    lista = [linha for linha in dict if linha["Materia"] == "rdsf"]
 
     with col2:    
         materia = st.selectbox("selecione um assunto",lista)    
