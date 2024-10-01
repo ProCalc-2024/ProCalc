041fe7fdac2f1cf111419c932efc0d62be63ab2c
ProCalc <167237786+ProCalc-2024@users.noreply.github.com>
2024-10-01 09:28:20 -0300

    Update save_questoes.py

--- save_questoes.py
+++ save_questoes.py
@@ -42,8 +42,76 @@ def inserir_ques():
     
     combined_data = pd.concat([existing_data, novo], ignore_index=True)
     
-    if st.button("Salvar"):   
+    with st.expander("Visualização da Questão"):
+        
+        st.subheader('', divider = 'gray')
+        
+        #numero de questoes
+        n=0
+        for linha in lista_ques:
+            n = n+1  
     
+        # embaralha as alternativas independente da questão 
+        lista = ["Alternativa_A","Alternativa_B","Alternativa_C","Alternativa_D","Alternativa_E"]
+        
+        if "embaralho" not in st.session_state:
+            
+            st.session_state["embaralho"] = np.random.choice(lista, 5, replace = False)
+    
+        if "ques" not in st.session_state:
+            st.session_state["save"] = {}
+            st.session_state["numero"] = 0
+            st.session_state["ques"] = np.random.randint(0,n)
+             
+        embaralho = st.session_state["embaralho"]
+        
+        # escolha de questão aleatoria
+        Questão = st.session_state["ques"]
+    
+        #comando da questão  
+        questao = lista_ques[Questão]    
+        st.write('')
+        st.write(questao["Enunciado"])
+            
+        st.subheader('', divider = 'gray')
+        
+        opções = [questao[embaralho[0]], questao[embaralho[1]], questao[embaralho[2]], questao[embaralho[3]], questao[embaralho[4]]]    
+        
+        alternativa = st.radio("", options = opções, index=None)
+            
+        st.session_state["resposta"] = questao["Alternativa_A"]
+    
+        butao = st.button("Submeter") 
+                
+        # salva a sequencia de questoes
+        resul.update(st.session_state["save"])                                                
+        
+        st.session_state["save"] = { st.session_state["numero"] + 1 : st.session_state["ques"] }
+        
+        sequencia = st.session_state["save"]
+            
+        resul.update(sequencia)
+    
+        # sequendcia de questões
+        # st.write(resul)
+        
+        st.session_state["save"] = resul
+    
+        resposta = alternativa == questao["Alternativa_A"]
+        
+        if butao and resposta:         
+            st.toast(':green-background[Resposta Certa]', icon='🎉')
+            lis = [ lin for lin in resul ]
+            new_ques(lista,n)
+            time.sleep(5)
+            st.rerun()
+    
+        elif butao and (resposta is False):
+            st.toast(':red-background[Resposta Errada]', icon="⚠️")
+        
+    if st.button("Salvar"):   
+        
+            
         conn.update(worksheet="Questões", data=combined_data)
        
         conn.read(
@@ -70,7 +138,7 @@ def inserir_assun():
     
     combined = pd.concat([existing, new], ignore_index=True) 
     
-    if st.button("Save"):
+    if st.button("visualizar questão"):
         
         conn.update(worksheet="Materias", data=combined)
        
