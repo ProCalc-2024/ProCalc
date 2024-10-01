2d4b1085663068ba9932407b5a77b7d8c4ed634f
ProCalc <167237786+ProCalc-2024@users.noreply.github.com>
2024-10-01 13:29:40 -0300

    Update questoes.py

--- questoes.py
+++ questoes.py
@@ -37,7 +37,7 @@ def read_questao():
     with col3:    
         # Pergunta ao usuário quantas Questões deseja criar
         numero = st.number_input("Quantas Questões você gostaria fazer?", min_value=1, max_value=20, value=1)
-    nun_tabs = numero
+        num_tabs = numero
     # Cria uma lista de nomes para as Questões
     tab_names = [f"Q{i + 1}" for i in range(num_tabs)]
     # Cria as abas dinamicamente
@@ -45,61 +45,64 @@ def read_questao():
         
     with col1:
         st.title("Perguntas")
-    
-    st.subheader('', divider = 'gray')
-    
-    #numero de questoes
-    n=0
-    for linha in lista_ques:
-        n = n+1  
 
-    # embaralha as alternativas independente da questão 
-    lista = ["Alternativa_A","Alternativa_B","Alternativa_C","Alternativa_D","Alternativa_E"]
-    
-    if "embaralho" not in st.session_state:
+    for i in range(num_tabs):
+            with tabs[i]:
         
-        st.session_state["embaralho"] = np.random.choice(lista, 5, replace = False)
-
-    if "ques" not in st.session_state:
-        st.session_state["save"] = {}
-        st.session_state["numero"] = 0
-        st.session_state["ques"] = np.random.randint(0,n)
-         
-    embaralho = st.session_state["embaralho"]
-    
-    # escolha de questão aleatoria
-    Questão = st.session_state["ques"]
-
-    #comando da questão  
-    questao = lista_ques[Questão]    
-    st.write('')
-    st.write(questao["Enunciado"])
-        
-    st.subheader('', divider = 'gray')
-    
-    opções = [questao[embaralho[0]], questao[embaralho[1]], questao[embaralho[2]], questao[embaralho[3]], questao[embaralho[4]]]    
-    
-    alternativa = st.radio("", options = opções, index=None)
-        
-    st.session_state["resposta"] = questao["Alternativa_A"]
-
-    butao = st.button("Submeter") 
-            
-    # salva a sequencia de questoes
-    resul.update(st.session_state["save"])                                                
-    
-    st.session_state["save"] = { st.session_state["numero"] + 1 : st.session_state["ques"] }
-    
-    sequencia = st.session_state["save"]
-        
-    resul.update(sequencia)
-
-    # sequendcia de questões
-    # st.write(resul)
-    
-    st.session_state["save"] = resul
-
-    resposta = alternativa == questao["Alternativa_A"]
+                    st.subheader('', divider = 'gray')
+                    
+                    #numero de questoes
+                    n=0
+                    for linha in lista_ques:
+                        n = n+1  
+                
+                    # embaralha as alternativas independente da questão 
+                    lista = ["Alternativa_A","Alternativa_B","Alternativa_C","Alternativa_D","Alternativa_E"]
+                    
+                    if "embaralho" not in st.session_state:
+                        
+                        st.session_state["embaralho"] = np.random.choice(lista, 5, replace = False)
+                
+                    if "ques" not in st.session_state:
+                        st.session_state["save"] = {}
+                        st.session_state["numero"] = 0
+                        st.session_state["ques"] = np.random.randint(0,n)
+                         
+                    embaralho = st.session_state["embaralho"]
+                    
+                    # escolha de questão aleatoria
+                    Questão = st.session_state["ques"]
+                
+                    #comando da questão  
+                    questao = lista_ques[Questão]    
+                    st.write('')
+                    st.write(questao["Enunciado"])
+                        
+                    st.subheader('', divider = 'gray')
+                    
+                    opções = [questao[embaralho[0]], questao[embaralho[1]], questao[embaralho[2]], questao[embaralho[3]], questao[embaralho[4]]]    
+                    
+                    alternativa = st.radio("", options = opções, index=None)
+                        
+                    st.session_state["resposta"] = questao["Alternativa_A"]
+                
+                    butao = st.button("Submeter") 
+                            
+                    # salva a sequencia de questoes
+                    resul.update(st.session_state["save"])                                                
+                    
+                    st.session_state["save"] = { st.session_state["numero"] + 1 : st.session_state["ques"] }
+                    
+                    sequencia = st.session_state["save"]
+                        
+                    resul.update(sequencia)
+                
+                    # sequendcia de questões
+                    # st.write(resul)
+                    
+                    st.session_state["save"] = resul
+                
+                    resposta = alternativa == questao["Alternativa_A"]
     
     if butao and resposta:         
         st.toast(':green-background[Resposta Certa]', icon='🎉')
