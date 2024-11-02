fc5e52e8af4954b8ec0b6c56f81f49255fe0f418
ProCalc <167237786+ProCalc-2024@users.noreply.github.com>
2024-11-02 17:08:34 -0300

    Update questoes.py

--- questoes.py
+++ questoes.py
@@ -42,11 +42,11 @@ def read_questao():
     
     # Cria uma lista de nomes para as questões
     tab_names = [f"Q{i + 1}" for i in range(numero)]
-    
+    tab_names.append("resposta")
     # Cria as abas dinamicamente
     tabs = st.tabs(tab_names)
-    resp = st.tabs("Respostas") 
-    st.write(resp)
+    
+    st.write(tab_names)
     with col1:
         st.title("Perguntas")
 
