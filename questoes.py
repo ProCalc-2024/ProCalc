f10d2426af09d3f87821579e31360ae21c9c24ba
ProCalc <167237786+ProCalc-2024@users.noreply.github.com>
2024-11-02 20:24:16 -0300

    Update questoes.py

--- questoes.py
+++ questoes.py
@@ -90,7 +90,7 @@ def read_questao():
             
             # Exibe as alternativas embaralhadas
             opcoes = [questao[embaralho[j]] for j in range(5)]
-            alternativa = st.radio("", options=opcoes, index=None, key = f"{i}")
+            alternativa = st.radio("", options=opcoes, index=None)
             
             st.session_state["resposta"] = questao["Alternativa_A"]
             resul.update(st.session_state["save"])                                                
@@ -106,7 +106,7 @@ def read_questao():
             else:
                 index = None
                 
-            alternativa = st.radio("", options=opcoes, index=index, key= i)
+            alternativa = st.radio("", options=opcoes, index=index, key= f"cha{i}")
             st.write(alternativa)
             
     # Botão de submissão
