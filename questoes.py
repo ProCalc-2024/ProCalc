df04db23f8ceaccb93481d8ed55d1afb57dfcefd
ProCalc <167237786+ProCalc-2024@users.noreply.github.com>
2024-11-02 18:41:08 -0300

    Update questoes.py

--- questoes.py
+++ questoes.py
@@ -102,7 +102,7 @@ def read_questao():
         
         with tabs[i+1]:
             for i in range(numero):
-                alternativa
+                st.write(alternativa)
             st.write(tab_names)
             
     # Botão de submissão
