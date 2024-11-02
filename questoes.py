1b8714ae413edefc9fdd5de92ca46c96c69d2b67
ProCalc <167237786+ProCalc-2024@users.noreply.github.com>
2024-11-02 18:44:49 -0300

    Update questoes.py

--- questoes.py
+++ questoes.py
@@ -100,7 +100,7 @@ def read_questao():
             st.session_state["save"] = resul
             resposta = alternativa == questao["Alternativa_A"]
         
-        with tabs[i+1]:
+       with tabs[i+1]:
             for i in range(numero):
                 st.write(alternativa)
             st.write(tab_names)
