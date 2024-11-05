89c4a44b46a851ad93c2f8ac8d2ec39cff4a12c0
ProCalc <167237786+ProCalc-2024@users.noreply.github.com>
2024-11-04 22:43:41 -0300

    Update save_questoes.py

--- save_questoes.py
+++ save_questoes.py
@@ -21,12 +21,12 @@ def inserir_ques():
     with col1:
         descricao = st.text_input("descrição")
 
-    enunciado = st.text_area("Enunciado", placeholder= "digite aqui o enunciado da questão")
-    letra_a = st.text_input("Resposta1", placeholder= "digite aqui a resposta correta") 
-    letra_b = st.text_input("Resposta2", placeholder= "digite aqui a resposta2")
-    letra_c = st.text_input("Resposta3", placeholder= "digite aqui a resposta3") 
-    letra_d = st.text_input("Resposta4", placeholder= "digite aqui a resposta4") 
-    letra_e = st.text_input("Resposta5", placeholder= "digite aqui a resposta5")
+    enunciado = st.text_area("Enunciado", placeholder= "digite aqui o enunciado da questão", key = "enunciado")
+    letra_a = st.text_input("Resposta1", placeholder= "digite aqui a resposta correta", key = "letra_a") 
+    letra_b = st.text_input("Resposta2", placeholder= "digite aqui a resposta2", key = "letra_b")
+    letra_c = st.text_input("Resposta3", placeholder= "digite aqui a resposta3", key = "letra_c") 
+    letra_d = st.text_input("Resposta4", placeholder= "digite aqui a resposta4", key = "letra_d") 
+    letra_e = st.text_input("Resposta5", placeholder= "digite aqui a resposta5", key = "letra_e")
 
     existing_data = conn.read(worksheet="Questões")
     novo = pd.DataFrame({
