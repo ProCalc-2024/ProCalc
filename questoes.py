ead321271f57050bfe689842c8bc077f48da9cde
ProCalc <167237786+ProCalc-2024@users.noreply.github.com>
2024-11-12 07:30:33 -0300

    Update questoes.py

--- questoes.py
+++ questoes.py
@@ -146,7 +146,7 @@ def read_questao():
         def new_questionario():
             st.session_state["botao"] = None
             st.session_state["disabled"] = False
-            st.rerun
+            st.rerun(scope = "app")
         # Mostra o botão somente se ele ainda não foi clicado
         if not st.session_state["botao"]:
             if st.button("Submeter", on_click=clicar_botao):
