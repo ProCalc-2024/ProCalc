be0172afbe1de32ad89043074e4fd8f42693ade5
ProCalc <167237786+ProCalc-2024@users.noreply.github.com>
2024-11-12 09:55:42 -0300

    Update questoes.py

--- questoes.py
+++ questoes.py
@@ -171,7 +171,7 @@ def read_questao():
             
         # Mostra o botão somente se ele ainda não foi clicado
         if not st.session_state["botao"]:
-            if st.button("Submeter", on_click = click1) and resultado2 :
+            if st.button("Submeter", on_click = click1):
                 clicar_botao
                   # O estado muda ao clicar, e o botão desaparece na próxima renderização
 
