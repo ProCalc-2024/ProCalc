78e7943b153a787545c157c8ade7d8d0e2443bed
ProCalc <167237786+ProCalc-2024@users.noreply.github.com>
2025-01-06 12:09:49 -0300

    Update questoes.py

--- questoes.py
+++ questoes.py
@@ -30,7 +30,7 @@ def read_questao():
     
         st.session_state["botao"] = False
         st.session_state["disabled"] = False
-        st.session_state.visibility = "visible"
+        st.session_state["disabledtime"] = False
           
     # lista de matérias
     lista = list(set(dict["Materia"]))
@@ -73,7 +73,7 @@ def read_questao():
             b = list(range(n))
             
             disabled2 = st.session_state["disabled"]
-            
+            disabled3 = st.session_state["disabledtime"]
             # Embaralha as alternativas
             lista = ["Alternativa_A", "Alternativa_B", "Alternativa_C", "Alternativa_D", "Alternativa_E"]
             if f"embaralho{i}{materia}" not in st.session_state:
@@ -215,8 +215,9 @@ def read_questao():
     with col1:
 
                 
-        min_question = st.slider("Timer do formulario", 0, 59, 0, label_visibility=st.session_state.visibility)        
-        hor_question = st.slider("", 0, 6, 0, label_visibility=st.session_state.visibility)
+        min_question = st.slider("Timer do formulario", 0, 59, 0,  disabled=disabled3)        
+        hor_question = st.slider("", 0, 6, 0, disabled=disabled3)
+        
         # Calcular o tempo total
         total_time = hor_question*60*60 + min_question*60
         if st.session_state["disabled"] == True or st.session_state["disabled"] == False:
