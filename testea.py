9507b9166333b12a1398b548cd1244c3f293f5f1
ProCalc <167237786+ProCalc-2024@users.noreply.github.com>
2025-02-02 09:01:35 -0300

    Update testea.py

--- testea.py
+++ testea.py
@@ -39,19 +39,12 @@ def inserir_usuario():
     
     combined_data = pd.concat([existing_data, novo], ignore_index=True)
     st.write(combined_data)        
-    st.session_state["resposta"] = questao["Alternativa_A"]
-    
+   
     butao = st.button("Submeter", key = "button_save_questoes") 
                 
-    # salva a sequencia de questoes
-
-    resposta = alternativa == questao["Alternativa_A"]
-        
-    if butao and resposta:         
-        st.toast(':green-background[Resposta Certa]', icon='🎉')
+    #st.toast(':green-background[Resposta Certa]', icon='🎉')
     
-    elif butao and (resposta is False):
-        st.toast(':red-background[Resposta Errada]', icon="⚠️")
+    #st.toast(':red-background[Resposta Errada]', icon="⚠️")
         
     if st.button("Cadastrar-se"):   
         
