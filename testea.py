992e6c14dc8ccc181169c924099cca46ebb5e4a0
ProCalc <167237786+ProCalc-2024@users.noreply.github.com>
2025-02-03 10:38:54 -0300

    Update testea.py

--- testea.py
+++ testea.py
@@ -31,6 +31,6 @@ def cadastrar_usuario():
 
             # Atualiza a planilha com os novos dados
             conn.update(worksheet="Usuários", data=df)
-            
+            conn.read(worksheet="Usuários", ttl="1s")
             st.success("Usuário cadastrado com sucesso!")
-            st.rerun
+            
