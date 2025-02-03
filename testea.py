09844bd85ef0daefd1847f779d901719c7e9571d
ProCalc <167237786+ProCalc-2024@users.noreply.github.com>
2025-02-03 10:58:53 -0300

    Update testea.py

--- testea.py
+++ testea.py
@@ -43,12 +43,10 @@ def cadastrar_usuario():
             conn.update(worksheet="Usuários", data=df)
 
             st.success("Usuário cadastrado com sucesso! Faça login agora.")
-            st.session_state["pagina"] = "Login"
-            st.rerun()
             
-    if st.button("Ir para Login"):
-        st.session_state["pagina"] = "Login"
-        st.rerun()
+            if st.button("Ir para Login"):
+                st.session_state["pagina"] = "Login"
+                st.rerun()
 
 def login_usuario():
     # Conexão com o Google Sheets
