67a7c488cdd0c7548ce226f0edd1a158d5ed8ff0
ProCalc <167237786+ProCalc-2024@users.noreply.github.com>
2025-02-17 10:11:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -46,8 +46,9 @@ if st.session_state["authentication_status"]:
     with col2:
         with tabs[0]:
                 testea.main()
-                if st.session_state["usuario"] != None:
-                        user = st.session_state["usuario"]
+                if st.session_state["pagina"] != None:
+                        user = st.session_state["pagina"]
+                        st.write("pagina")
                         
                 # Exibir a imagem no Streamlit
                 # st.image("grafico.png", caption='Imagem do Google Drive')
