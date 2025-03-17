639a4b0b6a93941138ef9be8ba1072cc7954b516
ProCalc <167237786+ProCalc-2024@users.noreply.github.com>
2025-03-17 09:33:14 -0300

    Update pag_inicial.py

--- pag_inicial.py
+++ pag_inicial.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
-st.title("Vídeo do YouTube no Streamlit")
-
-# URL do vídeo
-video_url = "https://youtu.be/3rxEC_TOfj8?si=Jc0r2CHQEgRjyabE"
-
-# Exibir como link clicável (com miniatura do YouTube)
-st.markdown(f'[![Assista no YouTube](https://youtu.be/3rxEC_TOfj8?si=Jc0r2CHQEgRjyabE/0.jpg)]({video_url})')
+def ensino():
+  st.title("Vídeo do YouTube no Streamlit")
+  
+  # URL do vídeo
+  video_url = "https://youtu.be/3rxEC_TOfj8?si=Jc0r2CHQEgRjyabE"
+  
+  # Exibir como link clicável (com miniatura do YouTube)
+  st.markdown(f'[![Assista no YouTube](https://youtu.be/3rxEC_TOfj8?si=Jc0r2CHQEgRjyabE/0.jpg)]({video_url})')
