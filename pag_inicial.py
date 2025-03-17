832fb4966c0337aa33ab88b3a7f53018435f43b3
ProCalc <167237786+ProCalc-2024@users.noreply.github.com>
2025-03-17 10:53:01 -0300

    Update pag_inicial.py

--- pag_inicial.py
+++ pag_inicial.py
@@ -2,10 +2,25 @@ import streamlit as st
 
 def ensino():
 
-  # URL do vídeo
-  video_url = "https://www.youtube.com/watch?v=d075cooe68s"
-
-  # Exibir como link clicável (com miniatura do YouTube)
-  st.markdown(f'[![Assista no YouTube](http://img.youtube.com/vi/d075cooe68s/0.jpg)]({video_url})')
-
-  st.video(video_url)
+  if "clicked" not in st.session_state:
+      st.session_state.clicked = False
+  
+  # HTML para criar um link clicável que atualiza a sessão
+  st.markdown(
+      """
+      <a href="?run=1" target="_self">
+          <img src="http://img.youtube.com/vi/dQw4w9WgXcQ/0.jpg" width="560">
+      </a>
+      """,
+      unsafe_allow_html=True
+  )
+  
+  # Detectar se a URL contém "run=1" (simula um clique)
+  query_params = st.query_params
+  if "run" in query_params:
+      st.session_state.clicked = True
+      st.experimental_rerun()
+  
+  # Exibir vídeo se foi clicado
+  if st.session_state.clicked:
+      st.video("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
