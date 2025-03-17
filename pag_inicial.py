85e85b7b61e707b2d70f6528416c348fa284355c
ProCalc <167237786+ProCalc-2024@users.noreply.github.com>
2025-03-17 09:30:14 -0300

    Create pag_inicial.py

---
+++ pag_inicial.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Vídeo do YouTube no Streamlit")
+
+# URL do vídeo
+video_url = "https://youtu.be/3rxEC_TOfj8?si=Jc0r2CHQEgRjyabE"
+
+# Exibir como link clicável (com miniatura do YouTube)
+st.markdown(f'[![Assista no YouTube](https://youtu.be/3rxEC_TOfj8?si=Jc0r2CHQEgRjyabE/0.jpg)]({video_url})')
