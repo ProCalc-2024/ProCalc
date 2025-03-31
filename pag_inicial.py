413abd270aa7ef0824ea2b879b1c3762bdc96e26
ProCalc <167237786+ProCalc-2024@users.noreply.github.com>
2025-03-31 11:50:19 -0300

    Update pag_inicial.py

--- pag_inicial.py
+++ pag_inicial.py
@@ -4,10 +4,10 @@ from extra_streamlit_components import CookieManager
 def ensino():
 
   # URL do vídeo
-  video_url = "https://www.youtube.com/watch?v=d075cooe68s"
+  #video_url = "https://www.youtube.com/watch?v=d075cooe68s"
 
   # Exibir como link clicável (com miniatura do YouTube)
-  st.markdown(f'[![Assista no YouTube](http://img.youtube.com/vi/d075cooe68s/0.jpg)]({video_url})')
+  #st.markdown(f'[![Assista no YouTube](http://img.youtube.com/vi/d075cooe68s/0.jpg)]({video_url})')
 
   #st.video(video_url)
 
@@ -16,6 +16,46 @@ def ensino():
   # Inicializa o gerenciador de cookies
   
   # Inicializa o CookieManager uma vez por sessão
+
+  # Inicializa o CookieManager uma vez por sessão
+  if 'cookie_manager' not in st.session_state:
+      st.session_state.cookie_manager = CookieManager()
+  cookie_manager = st.session_state.cookie_manager
+  
+  # Verifica o cookie ao carregar a página
+  if 'user_logged_in' not in st.session_state:
+      user_cookie = cookie_manager.get('user_auth')
+      if user_cookie:
+          st.session_state.user_logged_in = True
+          st.session_state.username = user_cookie
+      else:
+          st.session_state.user_logged_in = False
+  
+  # Página de login
+  if not st.session_state.user_logged_in:
+      st.title("Login")
+      username = st.text_input("Usuário")
+      password = st.text_input("Senha", type="password")
+      
+      if st.button("Entrar"):
+          if username == "admin" and password == "123":  # Simulação de autenticação
+              cookie_manager.set(
+                  'user_auth', 
+                  username, 
+                  max_age=86400,
+                  path="/"
+              )
+              st.session_state.user_logged_in = True
+              st.session_state.username = username
+              st.rerun()  # Recarrega para aplicar o cookie
+          else:
+              st.error("Credenciais inválidas")
+  else:
+      st.title(f"Bem-vindo, {st.session_state.username}!")
+      if st.button("Sair"):
+          cookie_manager.delete('user_auth')
+          st.session_state.clear()
+          st.rerun()
 
           
 
