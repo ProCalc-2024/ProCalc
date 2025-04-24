fd08350effff0ce18c4ad81337437171c64414df
ProCalc <167237786+ProCalc-2024@users.noreply.github.com>
2025-04-24 11:21:37 -0300

    Update testea.py

--- testea.py
+++ testea.py
@@ -130,20 +130,20 @@ def aulas():
     
     n = len(lista_ques)
     
-   with st.container():
-    col1, col2, col3 = st.columns(3)
+    with st.container():
+        col1, col2, col3 = st.columns(3)
 
-    with col1:
-        st.write("Conteúdo da coluna 1")
-        st.button("Botão 1")
+        with col1:
+            st.write("Conteúdo da coluna 1")
+            st.button("Botão 1")
 
-    with col2:
-        st.write("Conteúdo da coluna 2")
-        st.button("Botão 2")
+        with col2:
+            st.write("Conteúdo da coluna 2")
+            st.button("Botão 2")
 
-    with col3:
-        st.write("Conteúdo da coluna 3")
-        st.button("Botão 3")     
+        with col3:
+            st.write("Conteúdo da coluna 3")
+            st.button("Botão 3")     
     
         
     
