b3202ddde0cd7a634447d6ec40afb0756d0b50da
ProCalc <167237786+ProCalc-2024@users.noreply.github.com>
2025-05-15 09:29:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -69,6 +69,17 @@ if st.session_state["pagina"] == "Log":
                     #testea.aulas()
                         st.subheader("Em Construção")
 
+                        # Centraliza tudo com markdown
+                        st.markdown(
+                            """
+                            <div style="text-align: center; margin-top: 100px;">
+                                <h1 style="font-size: 60px;">🚧 Em Construção 🚧</h1>
+                                <p style="font-size: 24px;">Estamos trabalhando para trazer novidades em breve.</p>
+                            </div>
+                            """,
+                            unsafe_allow_html=True
+                        )
+
         else:
                 with tabs[1]:
 
