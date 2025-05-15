fd79abd1d6cb1be664503476488efb70721f3cc6
ProCalc <167237786+ProCalc-2024@users.noreply.github.com>
2025-05-15 09:54:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ import requests
 import testea
 import pag_inicial
 
-st.divider()
 def local_css(file_name):
         with open(file_name) as f:
             st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
